3d4ac99440cc5fe27a92b73ef0c69f8deb4e2753
clayton <98791427+clayton949@users.noreply.github.com>
2022-05-14 03:56:08 -0700

    dubstep mixer

---
+++ sound.js
@@ -0,0 +1,38 @@
+
+var x = document.getElementById("myAudio"); 
+function playAudio() { 
+  x.play(); 
+} 
+var a = document.getElementById("myAudio1"); 
+function playAudio1() { 
+  a.play(); 
+} 
+
+var b = document.getElementById("myAudio2"); 
+function playAudio2() { 
+  b.play(); 
+} 
+var c = document.getElementById("myAudio3"); 
+function playAudio3() { 
+  c.play(); 
+} 
+
+var d = document.getElementById("myAudio4"); 
+function playAudio4() { 
+  d.play(); 
+} 
+
+var e = document.getElementById("myAudio5"); 
+function playAudio5() { 
+  e.play(); 
+} 
+
+var f = document.getElementById("myAudio6"); 
+function playAudio2() { 
+  f.play(); 
+} 
+
+var g = document.getElementById("myAudio7"); 
+function playAudio7() { 
+  g.play(); 
+} 
